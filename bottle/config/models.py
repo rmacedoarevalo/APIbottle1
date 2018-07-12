47d71940878f673018f32cc6c6d85ac189178559
Я <rmacedoa@gmail.com>
2018-07-11 19:29:22 -0500

    Se agrego las tablas en python

--- bottle/config/models.py
+++ bottle/config/models.py
@@ -18,12 +18,65 @@ class Estacion(Base):
   altura = Column(Float)
   tipo_estacion_id = Column(Integer, ForeignKey('tipo_estaciones.id'))
 """
-class Usuario(Base):
-  __tablename__ = 'usuarios'
+#Tablas del sql en python
+class Usuarios(Base):
+  __tablename__ = 'Usuario'
   id = Column(Integer,primary_key=True)
   nombre= Column(String)
-  sistema_id = Column(Integer, ForeignKey('sistemas.id'))
+  skin = Coluns(Blob)
+  id_rep_skins = Column(Integer, ForeignKey('Repositorio_Skins.id'))
+  #sistema_id = Column(Integer, ForeignKey('sistemas.id'))
 
+class Seleccionar(Base)
+_tablename_ = 'seleccionar'
+id = Column(Integer,primary_key=True)
+id_usuario = Column(Integer, ForeignKey('Usuario.id'))
+id_mapas = Column(Integer, ForeignKey('Mapa.id'))
+score = Column(Integer)
+
+class Mapas(Base)
+_tablename_ = 'Mapa'
+id = Column(Integer,primary_key=True)
+id_nivel = Column(Integer,ForeignKey(nivel.id))
+foto = Column(Blob)
+
+
+class Niveles(Base)
+_tablename_ = 'Nivel'
+id = Column(Integer,primary_key=True)
+nombre = Column(String)
+numero_monedas = Column(Integer)
+numero_letras = Column(Integer)
+
+class Partidas(Base)
+_tablename_ = 'Partida'
+id = Column(Integer,primary_key=True)
+id_nivel = Column(Integer,ForeignKey('Nivel.id'))
+progreso_realizado = Column(Integer)
+monedas_recolectadas = Column(Integer)
+letras_recolectadas = Column(Integer)
+
+class Selector_Partidas(Base)
+_tablename_ = 'Selector_Partida'
+id = Column(Integer,primary_key=True)
+id_usuario = Column(Integer, ForeignKey('Usuario.id'))
+id_partida = Column(Integer, ForeignKey('Partida.id'))
+
+
+class Tienda(Base)
+_tablename_ = 'tienda'
+id = Column(Integer,primary_key=True)
+id_usuario = Column(Integer, ForeignKey('Usuario.id'))
+id_rep_skins = Column(Integer, ForeignKey('Repositorio_Skins.id'))
+monedas_disponibles = Column(Integer)
+
+class RepositorioSkins(Base)
+_tablename_ = 'Repositorio_Skins'
+id = Column(Integer,primary_key=True)
+id_usuario = Column(Integer, ForeignKey('Usuario.id'))
+id_tienda = Column(Integer, ForeignKey('tienda.id'))
+
+"""
 class Skins(Base):
   id = Column(Integer, primary_key=True)
   imagen=Column(file)
@@ -71,3 +124,6 @@ class ActualizarMonedas_Tienda(Base):
   id=Column(Integer,primary_key=True)
   monedas_finales_id= Column(Integer,ForeignKey('monedas_finales.id'))
   tienda_id= Column(Integer,ForeignKey('tienda.id'))
+"""
+
+"""
